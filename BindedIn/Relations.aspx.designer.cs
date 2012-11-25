88efcaf5227f4fa01d3e69e2a2843d13a7e17ef2
remimarenco <remi.marenco@gmail.com>
2012-11-26 00:03:27 +0100

    Mise en commentaire de la fonctionnalites d'affichage des relations

--- BindedIn/Relations.aspx.designer.cs
+++ BindedIn/Relations.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace BindedIn {
     
     
     public partial class Relations {
+        
+        /// <summary>
+        /// Contrôle Repeater1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+        
+        /// <summary>
+        /// Contrôle ObjectDataSource1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSource1;
     }
 }
