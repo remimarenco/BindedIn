d9f1b7f2147ca6170a44b2b3266560c86d71cab6
ghosterk <arthur@aryo.fr>
2012-11-28 21:27:10 +0100

    Rien de spécial.

--- BindedIn/Default.aspx.cs
+++ BindedIn/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 using Data;
 
 namespace BindedIn
@@ -21,7 +22,12 @@ namespace BindedIn
                 profile.FavoriteMovie = "Potatoe";
                 profile.Save();
             }
-            TextBox1.Text = profile.FavoriteMovie;
+            else
+            {
+                //TextBox1.Text = Membership.GetUser(User.Identity.Name).ProviderUserKey;
+                //var test = (Guid)Membership.GetUser(User.Identity.Name).ProviderUserKey;
+                TextBox1.Text = User.Identity.Name;
+            }
         }
 
         protected void ButUpdate_Click(object sender, EventArgs e)
