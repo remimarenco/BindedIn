53358ce38705956e477d90955d76b60803867b3a
remimarenco <remi.marenco@gmail.com>
2012-12-03 14:55:07 +0100

    Modification nom edmx

--- Business/FormationService.cs
+++ Business/FormationService.cs
@@ -24,7 +24,7 @@ namespace Business
                                  where uf.user.Equals(userId)
                                  where uf.formation.Equals(f.id)
                                  select f;
-        
+            
             return retour.ToList();
         }
 
